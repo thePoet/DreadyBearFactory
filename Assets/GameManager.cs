30bd8306d1642a254fa2e2cf5e3d364733cad74c
Riku Erkkilä <riku.erkkila@gmail.com>
2016-09-07 12:55:37 +0300

    Nicer layout

---
+++ Assets/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject title;
+	public GameObject startGamePrompt;
+	public GameObject[] levelInstructions;
+
+	int activeLevel;
+
+	void Start()
+	{
+
+	}
+
+	void OnStartGameClicked()
+	{
+		title.SetActive(false);
+		startGamePrompt.SetActive(false);
+		activeLevel = 1;
+		Invoke("TitleForLevel", 1f);
+	}
+
+	void TitleForLevel()
+	{
+		levelInstructions[activeLevel].SetActive(true);
+	}
+
+	void StartLevel()
+	{
+		foreach( GameObject levelInst in levelInstructions)
+			levelInst.SetActive(false);
+		
+	}
+
+
+
+}
