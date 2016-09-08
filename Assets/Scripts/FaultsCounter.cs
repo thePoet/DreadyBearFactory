05661dfb7916a2f6cd67afe6eae19cc6792140e6
Riku Erkkilä <riku.erkkila@gmail.com>
2016-09-08 10:42:26 +0300

    GitTest

---
+++ Assets/Scripts/FaultsCounter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaultsCounter : MonoBehaviour {
+
+	int count;
+	public GameObject[] bearLocations;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
